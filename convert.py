2c8ef250841335be08a7ce0615ec3913185e6ef7
ehlke-hepworth <157199273+ehlke-hepworth@users.noreply.github.com>
2024-04-16 12:11:06 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,20 +1,12 @@
-import comtypes.client
+import win32com.client
 import os
 
-def ppt_to_pdf(input_file_path, output_file_path, formatType=32):
-    powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
-    powerpoint.Visible = 1
+x = os.path.realpath("Capability Assessment Report.pptx")
+y = os.getcwd() + "Capability Assessment Reports.pdf"
 
-    if os.path.isabs(input_file_path):
-        input_file_path = os.path.abspath(input_file_path)
-    if not os.path.isabs(output_file_path):
-        output_file_path = os.path.abspath(output_file_path)
-
-    deck = powerpoint.Presentations.Open(input_file_path)
-    deck.SaveAs(output_file_path, formatType)  # formatType = 32 for PDF
-    deck.Close()
-    powerpoint.Quit()
-
-ppt_file = "Capability Assessment Report.pptx"
-pdf_file = "Capability Assessment Report.pdf"
-ppt_to_pdf(ppt_file, pdf_file)
+powerpoint = win32com.client.gencache.EnsureDispatch('PowerPoint.Application')
+powerpoint.Visible = True
+pdf = powerpoint.Presentations.Open(x)
+pdf.SaveAs(y, FileFormat = 32)
+pdf.Close()
+powerpoint.Quit()
